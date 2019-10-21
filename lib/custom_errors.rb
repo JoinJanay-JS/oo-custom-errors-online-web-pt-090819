2ea37a3ba5c6861146f087e5a2969a3fea36e4ba
Janay Bailey <tjanay87@gmail.com>
2019-10-21 20:57:59 +0000

    Done.

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -12,6 +12,18 @@ class Person
 
 end
 
+  class PartnerError < StandardError
+    #Raise the PartnerError when the argument passed into get_married
+    #is not an instance of the Person class
+    def message
+      "you must give the get_married method an argument of
+      an instance of the person class!"
+    end
+  end
+
+end
+
+
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
